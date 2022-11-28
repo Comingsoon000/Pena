b34ba3a299ce1a8dc2359c084eb37c3f1ad22a4c
Konstantin Dyakov <konstantin.d@fora-soft.com>
2022-11-28 19:49:41 +0300

    feat: add translations

---
+++ src/translations/en/index.ts
@@ -0,0 +1,15 @@
+import introduction from "./introduction.json";
+import services from "./services.json";
+import info from "./info.json";
+import aboutCompany from "./aboutCompany.json";
+import conclusion from "./conclusion.json";
+import footer from "./footer.json";
+
+export default {
+  introduction,
+  services,
+  info,
+  aboutCompany,
+  conclusion,
+  footer
+};
